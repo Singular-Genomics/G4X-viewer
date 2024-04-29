68d8b11de5255634ba895a14c1d8f0d06ee3f447
Pawel Dabrowski <rullox13@gmail.com>
2024-04-29 14:51:25 +0200

    fix: update button name

--- src/components/ViewController/DropzoneButton/DropzoneButton.tsx
+++ src/components/ViewController/DropzoneButton/DropzoneButton.tsx
@@ -1,5 +1,5 @@
-import { Button } from '@mui/material';
-import { useDropzone } from '../../../hooks/useDropzone';
+import { Button } from "@mui/material";
+import { useDropzone } from "../../../hooks/useDropzone";
 
 export default function DropzoneButton() {
   const { getRootProps, getInputProps } = useDropzone();
@@ -12,22 +12,22 @@ export default function DropzoneButton() {
       size="small"
       {...getRootProps()}
     >
-      <input {...getInputProps({ accept: '.tif, .tiff' })} />
-      Choose a file
+      <input {...getInputProps({ accept: ".tif, .tiff" })} />
+      Upload image file
     </Button>
   );
 }
 
 const sx = {
   dropDownButton: {
-    borderStyle: 'dashed',
-    width: '100%',
-    height: '40px',
-    borderColor: 'rgba(0, 177, 164, 1)',
-    color: 'rgba(0, 177, 164, 1)',
-    '&:hover': {
-      borderColor: 'rgba(0, 177, 164, 1)',
-      backgroundColor: 'rgba(0, 177, 164, 0.2)',
-    }
-  }
-}
+    borderStyle: "dashed",
+    width: "100%",
+    height: "40px",
+    borderColor: "rgba(0, 177, 164, 1)",
+    color: "rgba(0, 177, 164, 1)",
+    "&:hover": {
+      borderColor: "rgba(0, 177, 164, 1)",
+      backgroundColor: "rgba(0, 177, 164, 0.2)",
+    },
+  },
+};
