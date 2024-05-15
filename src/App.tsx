6ce1a89203c6b7c864da5888665e0e198dfcf80b
Mateusz Zduniuk <matzduniuk@gmail.com>
2024-05-15 16:00:10 +0200

    feat: remove the legacy demo data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
 import AvivatorTs from "./components/AvivatorTs";
 import { SnackbarProvider } from "notistack";
 
-// const resolveSource = (sourceQueryParam: string): ViewerSourceType => {
-//   if (sourceQueryParam) {
-//     return {
-//       urlOrFile: sourceQueryParam,
-//       description: sourceQueryParam.split("?")[0].split("/").slice(-1)[0],
-//     };
-//   }
-
-//   return {
-//     ...demoSources[6],
-//     isDemoImage: true,
-//   };
-// };
-
 export const App = () => {
-  // const query = new URLSearchParams(window.location.search);
-  // const source = resolveSource(query.get("image_url") ?? "");
-
   return (
     <SnackbarProvider
       anchorOrigin={{
