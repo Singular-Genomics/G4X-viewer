77da8c47ef8c31e32b82f7c8d74810d62c37976e
Mateusz Zduniuk <matzduniuk@gmail.com>
2024-05-17 17:36:18 +0200

    feat: add experimental metadata layer toggles to settings store

--- src/stores/MetadataLayerStore/MetadataLayerStore.ts
+++ src/stores/MetadataLayerStore/MetadataLayerStore.ts
@@ -3,11 +3,15 @@ import { MetadataLayerStore, MetadataLayerStoreValues } from "./MetadataLayerSto
 
 const DEFAULT_METADATA_LAYER_STORE_VALUES: MetadataLayerStoreValues = {
   isMetadataLayerOn: true,
+  showTilesBoundries: false,
+  showTilesData: false,
   pointSize: 1.5,
 }
 
 export const useMetadataLayerStore = create<MetadataLayerStore>((set) => ({
   ...DEFAULT_METADATA_LAYER_STORE_VALUES,
   toggleMetadataLayer: () => set((store) => ({isMetadataLayerOn: !store.isMetadataLayerOn})),
+  toggleTileBoundries: () => set((store) => ({showTilesBoundries: !store.showTilesBoundries})),
+  toggleTileData: () => set((store) => ({showTilesData: !store.showTilesData})),
   setPointSize: (newPointSize) => set({ pointSize: newPointSize }),
 }))
--- src/stores/MetadataLayerStore/MetadataLayerStore.types.ts
+++ src/stores/MetadataLayerStore/MetadataLayerStore.types.ts
@@ -2,10 +2,14 @@ export type MetadataLayerStore = MetadataLayerStoreValues & MetadataLayerStoreMe
 
 export type MetadataLayerStoreValues = {
   isMetadataLayerOn: boolean;
+  showTilesBoundries: boolean;
+  showTilesData: boolean;
   pointSize: number;
 }
 
 export type MetadataLayerStoreMethods = {
   toggleMetadataLayer: () => void;
+  toggleTileBoundries: () => void;
+  toggleTileData: () => void;
   setPointSize: (newPointSize: number) => void;
 }
