cca87d4a1739c6436ffe88831d496eab16c92871
Mateusz Zduniuk <matzduniuk@gmail.com>
2024-05-21 11:53:50 +0200

    feat: add warning icon for disabled metadata view layer

--- src/components/ViewController/MetadataLayerSection/MetadataLayerSection.tsx
+++ src/components/ViewController/MetadataLayerSection/MetadataLayerSection.tsx
@@ -1,24 +1,64 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Tooltip, Typography, tooltipClasses } from "@mui/material";
 import { PointSizeSlider } from "./PointSizeSlider";
 import { ExperimentalToggles } from "./ExperimentalToggles";
 import { PointFilter } from "./PointsFilter/PointFilter";
+import WarningIcon from "@mui/icons-material/Warning";
+import { useMetadataLayerStore } from "../../../stores/MetadataLayerStore";
+import { useShallow } from "zustand/react/shallow";
 
-export const MetadataLayerSection = () => (
-  <Box sx={sx.sectionContainer}>
-    <Box>
-      <Typography sx={sx.subsectionTitle}>Point Size</Typography>
-      <PointSizeSlider />
-    </Box>
-    <Box>
-      <Typography sx={sx.subsectionTitle}>Experimental Options</Typography>
-      <ExperimentalToggles />
-    </Box>
-    <Box>
-      <Typography sx={sx.subsectionTitle}>Point Filters</Typography>
-      <PointFilter/>
-    </Box>
-  </Box>
+const DisabledLayerWarning = () => (
+  <Tooltip
+    title="Metadata layer is disabled"
+    placement="top"
+    arrow
+    slotProps={{ popper: { sx: sx.warningTooltip } }}
+  >
+    <WarningIcon sx={sx.warningIcon} />
+  </Tooltip>
 );
+
+export const MetadataLayerSection = () => {
+  const [
+    isMetadataLayerOn,
+    isGeneNameFilterActive,
+    showTilesBoundries,
+    showTilesData,
+  ] = useMetadataLayerStore(
+    useShallow((store) => [
+      store.isMetadataLayerOn,
+      store.isGeneNameFilterActive,
+      store.showTilesBoundries,
+      store.showTilesData,
+    ])
+  );
+
+  return (
+    <Box sx={sx.sectionContainer}>
+      <Box>
+        <Typography sx={sx.subsectionTitle}>Point Size</Typography>
+        <PointSizeSlider />
+      </Box>
+      <Box>
+        <Box sx={sx.subsectionWrapper}>
+          <Typography sx={sx.subsectionTitle}>Experimental Options</Typography>
+          {!isMetadataLayerOn && (showTilesBoundries || showTilesData) && (
+            <DisabledLayerWarning />
+          )}
+        </Box>
+        <ExperimentalToggles />
+      </Box>
+      <Box>
+        <Box sx={sx.subsectionWrapper}>
+          <Typography sx={sx.subsectionTitle}>Point Filters</Typography>
+          {!isMetadataLayerOn && isGeneNameFilterActive && (
+            <DisabledLayerWarning />
+          )}
+        </Box>
+        <PointFilter />
+      </Box>
+    </Box>
+  );
+};
 
 const sx = {
   sectionContainer: {
@@ -26,9 +66,23 @@ const sx = {
     flexDirection: "column",
     gap: "8px",
   },
+  subsectionWrapper: {
+    display: "flex",
+    gap: "8px",
+  },
   subsectionTitle: {
     fontWeight: 700,
     paddingLeft: "8px",
     marginBottom: "8px",
+  },
+  warningIcon: {
+    color: "#b19218",
+    border: "",
+  },
+  warningTooltip: {
+    [`&.${tooltipClasses.popper}[data-popper-placement*="top"] .${tooltipClasses.tooltip}`]:
+      {
+        marginBottom: "0px",
+      },
   },
 };
--- src/components/ViewController/MetadataLayerSection/PointsFilter/PointFilter.tsx
+++ src/components/ViewController/MetadataLayerSection/PointsFilter/PointFilter.tsx
@@ -1,10 +1,9 @@
-import { Box } from "@mui/material";
 import { PointFilterOptions } from "./PointFilterOptions";
 import { PointFiltersTable } from "./PointFiltersTable";
 
 export const PointFilter = () => (
-  <Box>
+  <>
     <PointFilterOptions />
     <PointFiltersTable />
-  </Box>
+  </>
 );
