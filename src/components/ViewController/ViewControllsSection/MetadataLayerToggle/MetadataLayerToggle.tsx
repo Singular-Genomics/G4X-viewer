919fecc2499d33ca60d4f0535f3679c2aadc9b11
Mateusz Zduniuk <matzduniuk@gmail.com>
2024-05-23 08:51:52 +0200

    chore: update checkbox component to use MUi control label

--- src/components/ViewController/ViewControllsSection/MetadataLayerToggle/MetadataLayerToggle.tsx
+++ src/components/ViewController/ViewControllsSection/MetadataLayerToggle/MetadataLayerToggle.tsx
@@ -1,6 +1,6 @@
 import { useShallow } from "zustand/react/shallow";
 import { useViewerStore } from "../../../../stores/ViewerStore";
-import { Grid } from "@mui/material";
+import { FormControlLabel } from "@mui/material";
 import { GxCheckbox } from "../../../../shared/components/GxCheckbox";
 
 export const MetadataLayerToggle = () => {
@@ -9,22 +9,15 @@ export const MetadataLayerToggle = () => {
   );
 
   return (
-    <Grid
-      container
-      direction="row"
-      justifyContent="flex-start"
-      alignItems="center"
-    >
-      <Grid item xs={2}>
+    <FormControlLabel
+      label="Metadata Layer"
+      control={
         <GxCheckbox
           onChange={toggleMetadataLayer}
           checked={isMetadataLayerOn}
           disableTouchRipple
         />
-      </Grid>
-      <Grid item>
-        Metadata Layer
-      </Grid>
-    </Grid>
+      }
+    />
   );
 };
--- src/components/ViewController/ViewControllsSection/OverviewToggle/OverviewToggle.tsx
+++ src/components/ViewController/ViewControllsSection/OverviewToggle/OverviewToggle.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { FormControlLabel } from "@mui/material";
 import { useViewerStore } from "../../../../stores/ViewerStore/ViewerStore";
 import { useShallow } from "zustand/react/shallow";
 import { GxCheckbox } from "../../../../shared/components/GxCheckbox";
@@ -9,22 +9,15 @@ export const OverviewToggle = () => {
   );
 
   return (
-    <Grid
-      container
-      direction="row"
-      justifyContent="flex-start"
-      alignItems="center"
-    >
-      <Grid item xs={2}>
+    <FormControlLabel
+      label="Overview"
+      control={
         <GxCheckbox
           onChange={toggleOverview}
           checked={isOverviewOn}
           disableTouchRipple
         />
-      </Grid>
-      <Grid item>
-        Overview
-      </Grid>
-    </Grid>
+      }
+    />
   );
 };
