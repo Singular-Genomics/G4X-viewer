c8028f6ab69853c3af39236612f7369ca4e5716d
Mateusz Zduniuk <matzduniuk@gmail.com>
2024-05-29 19:12:30 +0200

    feat: add temporary fix to tooltip errors

--- src/components/PictureInPictureViewerAdapter/PictureInPictureViewerAdapter.helpers.ts
+++ src/components/PictureInPictureViewerAdapter/PictureInPictureViewerAdapter.helpers.ts
@@ -1,18 +1,24 @@
-export const getCutomTooltp = ({ object }: { object: any }) => {
+export const getCustomTooltp = ({ object }: { object: any }) => {
   return (
     object && {
       html: `
       <div style="display: flex; gap: 10px;">
         <div style="font-weight: 300;">
-          <p>Position</p>
+          ${"position" in object ? "<p>Position</p>" : ""}
           <p>Color</p>
-          <p>Gene Name: </p>
+          <p>${('geneName' in object) ? 'Gene Name:' : 'Cell name: ' }</p>
           <p>Cell ID: </p>
         </div>
         <div style="font-weight: 500;">
-          <p>X ${object.position[0].toFixed(2)} Y ${object.position[1].toFixed(2)}</p>
+          ${
+            "position" in object
+              ? `<p>X ${object.position[0].toFixed(
+                  2
+                )} Y ${object.position[1].toFixed(2)}</p>`
+              : ""
+          }
           <p>R ${object.color[0]} G ${object.color[1]} B ${object.color[2]}</p>
-          <p>${object.geneName}</p>
+          <p>${('geneName' in object) ? object.geneName : object.cellName}</p>
           <p>${object.cellId}</p>
         </div>
       </div>
@@ -20,11 +26,11 @@ export const getCutomTooltp = ({ object }: { object: any }) => {
       style: {
         padding: "0px 15px 0px 10px",
         backgroundColor: "rgba(51, 51, 51)",
-        borderColor: '#ababab',
+        borderColor: "#ababab",
         border: "2px solid",
-        borderRadius: '10px',
-        fontSize: '0.8em',
-        color: '#FFF',
+        borderRadius: "10px",
+        fontSize: "0.8em",
+        color: "#FFF",
       },
     }
   );
