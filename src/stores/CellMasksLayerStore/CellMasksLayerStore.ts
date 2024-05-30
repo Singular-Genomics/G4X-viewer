124f2f0cffdb61961f8a02c6fc755352d0cf8988
Mateusz Zduniuk <matzduniuk@gmail.com>
2024-05-30 10:39:06 +0200

    Add control values to cell masks layer store

--- src/stores/CellMasksLayerStore/CellMasksLayerStore.ts
+++ src/stores/CellMasksLayerStore/CellMasksLayerStore.ts
@@ -5,9 +5,15 @@ const CELL_MASKS_STORE_DEFAULT_VALUES: CellMasksLayerStoreValues = {
   cellMasksData: null,
   fileName: '',
   isCellLayerOn: true,
+  isCellFillOn: true,
+  isCellStrokeOn: true,
+  cellStrokeWidth: 5,
+  cellFillOpacity: 0.2,
 }
 
 export const useCellMasksLayerStore = create<CellMasksLayerStore>((set) => ({
   ...CELL_MASKS_STORE_DEFAULT_VALUES,
   toggleCellLayer: () => set((store) => ({isCellLayerOn: !store.isCellLayerOn})),
+  toggleCellFill: () => set((store) => ({isCellFillOn: !store.isCellFillOn})),
+  toggleCellStroke: () => set((store) => ({isCellStrokeOn: !store.isCellStrokeOn})),
 }))
--- src/stores/CellMasksLayerStore/CellMasksLayerStore.types.ts
+++ src/stores/CellMasksLayerStore/CellMasksLayerStore.types.ts
@@ -4,8 +4,14 @@ export type CellMasksLayerStoreValues = {
   cellMasksData: Uint8Array | null;
   fileName: string;
   isCellLayerOn: boolean;
+  isCellStrokeOn: boolean;
+  isCellFillOn: boolean;
+  cellStrokeWidth: number;
+  cellFillOpacity: number;
 }
 
 export type CellMasksLayerStoreMethods = {
   toggleCellLayer: () => void;
+  toggleCellStroke: () => void;
+  toggleCellFill: () => void;
 }
