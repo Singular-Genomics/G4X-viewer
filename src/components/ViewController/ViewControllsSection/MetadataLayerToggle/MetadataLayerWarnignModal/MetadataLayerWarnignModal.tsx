5875cb1b06d9a517671921a68820a5b6efa6563d
Mateusz Zduniuk <mzduniuk@airnauts.com>
2024-09-10 20:12:21 +0200

    chore: setup warning modal for disabling tiled layer view

---
+++ src/components/ViewController/ViewControllsSection/MetadataLayerToggle/MetadataLayerWarnignModal/MetadataLayerWarnignModal.tsx
@@ -0,0 +1,126 @@
+import {
+  Box,
+  Button,
+  FormControlLabel,
+  Modal,
+  Theme,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import WarningRoundedIcon from "@mui/icons-material/WarningRounded";
+import { GxCheckbox } from "../../../../../shared/components/GxCheckbox";
+import { useRef } from "react";
+
+export const MetadataLayerWarnignModal = () => {
+  const theme = useTheme();
+  const sx = styles(theme);
+  const checkboxRef = useRef<HTMLButtonElement>(null);
+
+  return (
+    <Modal open={true}>
+      <Box sx={sx.modalContainer}>
+        <Box sx={sx.headerWrapper}>
+          <WarningRoundedIcon sx={sx.warningIcon} />
+          <Typography sx={sx.warnignTitle}>warning</Typography>
+        </Box>
+        <Box sx={sx.modalContentWrapper}>
+          <Typography sx={sx.modalContentText}>
+            You are about to disable the tiled layer view.
+            <br />
+            This operation demands significant computational resources and might
+            cause the application to crash. Perform this operation if:
+            <ul>
+              <li>
+                Your PC is equiped with high-end components (mainly GPU and CPU)
+                and allows for hardwere acceleration.
+              </li>
+              <li>Your transcript dataset contains less than 50,000 points.</li>
+            </ul>
+          </Typography>
+          <Box sx={sx.modalButtonsWrapper}>
+            <Button sx={{ ...sx.modalButtonBase, ...sx.continueButton }}>
+              Confirm
+            </Button>
+            <Button sx={{ ...sx.modalButtonBase, ...sx.cancelButton }}>
+              Cancel
+            </Button>
+          </Box>
+          <hr />
+          <FormControlLabel
+            sx={sx.chechboxWrapper}
+            label="Don't ask me again"
+            control={<GxCheckbox ref={checkboxRef} />}
+          />
+        </Box>
+      </Box>
+    </Modal>
+  );
+};
+
+const styles = (theme: Theme) => ({
+  modalContainer: {
+    width: "fit-content",
+    position: "absolute",
+    background:
+      "linear-gradient(90deg, rgba(251,0,0,1) 0%, rgba(135,0,0,1) 63%, rgba(96,0,0,1) 100%)",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    borderRadius: "16px",
+    border: "1px solid rgba(125, 125, 125, 1)",
+    overflow: "hidden",
+  },
+  headerWrapper: {
+    display: "flex",
+    alignItems: "center",
+    gap: "16px",
+    padding: "8px",
+  },
+  warningIcon: {
+    height: "64px",
+    width: "64px",
+    color: theme.palette.gx.primary.white,
+  },
+  warnignTitle: {
+    fontSize: "32px",
+    fontWeight: "bold",
+    color: theme.palette.gx.primary.white,
+    textTransform: "uppercase",
+  },
+  modalContentWrapper: {
+    padding: "16px",
+    background: theme.palette.gx.lightGrey[700],
+  },
+  modalContentText: {
+    fontWeight: "bold",
+  },
+  modalButtonsWrapper: {
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    margin: "48px 0px 16px",
+  },
+  modalButtonBase: {
+    fontWeight: "bold",
+    padding: "4px 32px",
+    transition: "all 0.2s",
+    "&:hover": {
+      transform: "translateY(-4px)",
+      boxShadow: "0px 5px 8px rgba(0, 0, 0, 0.3)",
+    },
+  },
+  continueButton: {
+    background:
+      "linear-gradient(90deg, rgba(251,0,0,1) 0%, rgba(135,0,0,1) 100%)",
+    color: theme.palette.gx.primary.white,
+  },
+  cancelButton: {
+    color: theme.palette.gx.primary.black,
+    borderWidth: "2px",
+    borderStyle: "solid",
+    borderColor: theme.palette.gx.primary.black,
+  },
+  chechboxWrapper: {
+    marginLeft: "16px",
+  },
+});
