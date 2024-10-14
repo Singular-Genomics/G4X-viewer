f8a8c7953637de021ea2f9d671dbdf0ad6d5401d
Mateusz Zduniuk <mzduniuk@airnauts.com>
2024-10-14 15:36:29 +0200

    chore: add a new H&E image store

---
+++ src/stores/HEImageStore/HEImageStore.ts
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+import { HEImageStore, HEImageStoreValues } from "./HEImageStore.types";
+
+const DEFAULT_VALUES: HEImageStoreValues = {
+  heImageSource: null,
+  loader: [{ labels: [], shape: [] }],
+  image: 0,
+  contrastLimits: [[0, 65535]],
+  selections: [{ z: 0, c: 0, t: 0 }],
+  opacity: 1,
+  isImageLoading: false,
+};
+
+export const useHEImageStore = create<HEImageStore>((set, get) => ({
+  ...DEFAULT_VALUES,
+  reset: () => set({ ...DEFAULT_VALUES }),
+  getLoader: () => {
+    const { loader, image } = get();
+    return Array.isArray(loader[0]) ? loader[image] : loader;
+  },
+}));
