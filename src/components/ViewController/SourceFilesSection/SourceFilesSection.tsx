4216cd849feef893d32d849273733c4ac583132b
Mateusz Zduniuk <mzduniuk@airnauts.com>
2024-10-30 15:59:57 +0100

    chore: add a warning modal when accessing single file upload mode

--- src/components/ViewController/SourceFilesSection/SourceFilesSection.tsx
+++ src/components/ViewController/SourceFilesSection/SourceFilesSection.tsx
@@ -2,40 +2,64 @@ import { Box, FormControlLabel } from "@mui/material";
 import TranscriptDropzoneButton from "./TranscriptDropzoneButton/TranscriptDropzoneButton";
 import ImageDropzoneButton from "./ImageDropzoneButton/ImageDropzoneButton";
 import { CellMasksDropzoneButton } from "./CellMasksDropzoneButton";
-import HEImageDropzoneButton from "./HEImageDropzoneButton/HEImageDropzoneButton";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { GxSwitch } from "../../../shared/components/GxSwitch";
 import CollectiveDropzoneButton from "./CollectiveDropzoneButton/CollectiveDropzoneButton";
+import { WarningModal } from "./WarningModal";
 
 export const SourceFilesSection = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [isSingleFileMode, setIsSingleFileMode] = useState(false);
+
+  const toggleSingleFileUpload = useCallback(() => {
+    const disableModal = localStorage.getItem(
+      "disableSingleFileUploadWarning_DSA"
+    );
+    if (disableModal || isSingleFileMode) {
+      setIsSingleFileMode((prev) => !prev);
+    } else {
+      setIsModalOpen(true);
+    }
+  }, [setIsSingleFileMode, isSingleFileMode]);
+
+  const onContinue = useCallback(() => {
+    setIsModalOpen(false);
+    setIsSingleFileMode(true);
+  }, []);
+
   return (
-    <Box>
-      <FormControlLabel
-        labelPlacement="end"
-        label={isSingleFileMode ? "Multi file uplaod" : "Single file upload"}
-        sx={{ paddingLeft: "8px", marginBottom: "8px" }}
-        control={
-          <GxSwitch
-            disableTouchRipple
-            checked={isSingleFileMode}
-            onChange={() => setIsSingleFileMode((prev) => !prev)}
-          />
-        }
+    <>
+      <Box>
+        <FormControlLabel
+          labelPlacement="end"
+          label={isSingleFileMode ? "Multi file uplaod" : "Single file upload"}
+          sx={{ paddingLeft: "8px", marginBottom: "8px" }}
+          control={
+            <GxSwitch
+              disableTouchRipple
+              checked={isSingleFileMode}
+              onChange={toggleSingleFileUpload}
+            />
+          }
+        />
+        {isSingleFileMode ? (
+          <Box>
+            <CollectiveDropzoneButton />
+          </Box>
+        ) : (
+          <Box sx={sx.sourceFilesSectionContainer}>
+            <ImageDropzoneButton />
+            <TranscriptDropzoneButton />
+            <CellMasksDropzoneButton />
+          </Box>
+        )}
+      </Box>
+      <WarningModal
+        onContinue={onContinue}
+        isOpen={isModalOpen}
+        handleClose={() => setIsModalOpen(false)}
       />
-      {isSingleFileMode ? (
-        <Box>
-          <CollectiveDropzoneButton />
-        </Box>
-      ) : (
-        <Box sx={sx.sourceFilesSectionContainer}>
-          <ImageDropzoneButton />
-          <HEImageDropzoneButton />
-          <TranscriptDropzoneButton />
-          <CellMasksDropzoneButton />
-        </Box>
-      )}
-    </Box>
+    </>
   );
 };
 
