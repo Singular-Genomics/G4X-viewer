39cde93d973123c1efa8d5126bb77dca93023d00
Mateusz Zduniuk <mzduniuk@airnauts.com>
2024-10-30 13:27:51 +0100

    chore: integrate the collective file upload button

--- src/components/ViewController/SourceFilesSection/SourceFilesSection.tsx
+++ src/components/ViewController/SourceFilesSection/SourceFilesSection.tsx
@@ -1,17 +1,43 @@
-import { Box } from "@mui/material";
+import { Box, FormControlLabel } from "@mui/material";
 import TranscriptDropzoneButton from "./TranscriptDropzoneButton/TranscriptDropzoneButton";
 import ImageDropzoneButton from "./ImageDropzoneButton/ImageDropzoneButton";
 import { CellMasksDropzoneButton } from "./CellMasksDropzoneButton";
 import HEImageDropzoneButton from "./HEImageDropzoneButton/HEImageDropzoneButton";
+import { useState } from "react";
+import { GxSwitch } from "../../../shared/components/GxSwitch";
+import CollectiveDropzoneButton from "./CollectiveDropzoneButton/CollectiveDropzoneButton";
 
-export const SourceFilesSection = () => (
-  <Box sx={sx.sourceFilesSectionContainer}>
-    <ImageDropzoneButton />
-    <HEImageDropzoneButton />
-    <TranscriptDropzoneButton />
-    <CellMasksDropzoneButton />
-  </Box>
-);
+export const SourceFilesSection = () => {
+  const [isSingleFileMode, setIsSingleFileMode] = useState(false);
+  return (
+    <Box>
+      <FormControlLabel
+        labelPlacement="end"
+        label={isSingleFileMode ? "Multi file uplaod" : "Single file upload"}
+        sx={{ paddingLeft: "8px", marginBottom: "8px" }}
+        control={
+          <GxSwitch
+            disableTouchRipple
+            checked={isSingleFileMode}
+            onChange={() => setIsSingleFileMode((prev) => !prev)}
+          />
+        }
+      />
+      {isSingleFileMode ? (
+        <Box>
+          <CollectiveDropzoneButton />
+        </Box>
+      ) : (
+        <Box sx={sx.sourceFilesSectionContainer}>
+          <ImageDropzoneButton />
+          <HEImageDropzoneButton />
+          <TranscriptDropzoneButton />
+          <CellMasksDropzoneButton />
+        </Box>
+      )}
+    </Box>
+  );
+};
 
 const sx = {
   sourceFilesSectionContainer: {
