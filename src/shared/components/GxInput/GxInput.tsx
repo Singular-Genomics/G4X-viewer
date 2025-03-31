83630690f9f2d368fc254612aee905376b31ad1f
Mateusz Zduniuk <matzduniuk@gmail.com>
2025-03-31 11:39:58 +0200

    chore: setup pre-styles GxInput shared component

---
+++ src/shared/components/GxInput/GxInput.tsx
@@ -0,0 +1,36 @@
+import { TextField, TextFieldProps, Theme, useTheme } from "@mui/material";
+
+export const GxInput = ({
+  children,
+  sx: customStyles,
+  ...rest
+}: TextFieldProps) => {
+  const theme = useTheme();
+  const sx = styles(theme);
+
+  return (
+    <TextField sx={{ ...customStyles, ...sx.input }} {...rest}>
+      {children}
+    </TextField>
+  );
+};
+
+const styles = (theme: Theme) => ({
+  input: {
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: theme.palette.gx.mediumGrey[100],
+      },
+      "&:hover fieldset": {
+        borderColor: theme.palette.gx.darkGrey[500],
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: theme.palette.gx.accent.greenBlue,
+        borderWidth: "1px",
+      },
+    },
+    "& .MuiFormLabel-root.Mui-focused": {
+      color: theme.palette.gx.accent.greenBlue,
+    },
+  },
+});
