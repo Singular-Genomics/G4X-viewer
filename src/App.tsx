b7b8ef5807a4c494bb94fd722005005f0b690f94
Mateusz Zduniuk <matzduniuk@gmail.com>
2025-08-07 11:20:03 +0200

    chore: configure the i18next package

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,20 @@ import G4XViewer from './components/G4XViewer';
 import { SnackbarProvider } from 'notistack';
 import { gxTheme } from './themes/theme';
 import { GxSnackbar } from './shared/components/GxSnackbar/GxSnackbar';
+import { initReactI18next } from 'react-i18next';
+import HttpBackend, { HttpBackendOptions } from 'i18next-http-backend';
+import i18n from 'i18next';
+
+i18n
+  .use(HttpBackend)
+  .use(initReactI18next)
+  .init<HttpBackendOptions>({
+    backend: {
+      loadPath: '/src/shared/locales/{{lng}}.json'
+    },
+    lng: 'en',
+    fallbackLng: 'en'
+  });
 
 export const App = () => {
   return (
