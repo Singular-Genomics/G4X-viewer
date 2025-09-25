47b8d28eadf4dff7de849df61b310cc0cce9d3f7
luh-sg <lhou@singulargenomics.com>
2025-09-25 15:51:13 -0700

    chore: clean up the style

--- src/components/ViewController/TranscriptLayerSection/PointsFilter/PointFiltersTable/ColorPickerCell.tsx
+++ src/components/ViewController/TranscriptLayerSection/PointsFilter/PointFiltersTable/ColorPickerCell.tsx
@@ -1,11 +1,13 @@
-import { ClickAwayListener, IconButton, MenuList, Paper, Popper, Tooltip } from '@mui/material';
+import { Theme, ClickAwayListener, IconButton, MenuList, Paper, Popper, Tooltip, alpha, useTheme } from '@mui/material';
 import LensIcon from '@mui/icons-material/Lens';
 import { useState, useRef } from 'react';
 import { useBinaryFilesStore } from '../../../../../stores/BinaryFilesStore';
-import { COLOR_PALLETE } from '../../../../../shared/constants';
-import { PointFiltersTableRowEntry } from './PointFiltersTable.types';
+import { type ColorPickerCellProps } from './PointFiltersTable.types';
+import { ColorPalette } from '../../../ChannelsSettingsSection/ChannelControllers/ChannelOptions/ColorPalette';
 
-export const ColorPickerCell = ({ row }: { row: PointFiltersTableRowEntry }) => {
+export const ColorPickerCell = ({ row }: ColorPickerCellProps) => {
+  const theme = useTheme();
+  const sx = styles(theme);
   const [isOpen, setIsOpen] = useState(false);
   const anchorRef = useRef(null);
   const setColormapConfig = useBinaryFilesStore((store) => store.setColormapConfig);
@@ -21,14 +23,14 @@ export const ColorPickerCell = ({ row }: { row: PointFiltersTableRowEntry }) => 
 
   return (
     <>
-      <Tooltip title={`RGB: ${row.color.join(' ')} - Click to change`}>
+      <Tooltip title="Click to change">
         <IconButton
           ref={anchorRef}
           onClick={(event) => {
             event.stopPropagation();
             setIsOpen((prev) => !prev);
           }}
-          sx={{ padding: '4px' }}
+          sx={sx.geneColorButton}
         >
           <LensIcon style={{ color: `rgb(${row.color})` }} />
         </IconButton>
@@ -37,37 +39,11 @@ export const ColorPickerCell = ({ row }: { row: PointFiltersTableRowEntry }) => 
         open={isOpen}
         anchorEl={anchorRef.current}
         placement="bottom-start"
-        style={{ zIndex: 1300 }}
       >
-        <Paper sx={{ backgroundColor: 'rgba(0, 0, 0, 0.75)', padding: '8px' }}>
+        <Paper sx={sx.paper}>
           <ClickAwayListener onClickAway={() => setIsOpen(false)}>
             <MenuList>
-              <div
-                style={{
-                  width: '120px',
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  alignItems: 'center',
-                  flexWrap: 'wrap',
-                  gap: '4px'
-                }}
-              >
-                {COLOR_PALLETE.map((color, index) => (
-                  <IconButton
-                    key={index}
-                    onClick={(event) => {
-                      event.stopPropagation();
-                      handleColorSelect(color);
-                    }}
-                    sx={{ padding: '3px', width: '24px', height: '24px' }}
-                  >
-                    <LensIcon
-                      fontSize="small"
-                      style={{ color: `rgb(${color})`, width: '20px', height: '20px' }}
-                    />
-                  </IconButton>
-                ))}
-              </div>
+              <ColorPalette handleColorSelect={handleColorSelect} />
             </MenuList>
           </ClickAwayListener>
         </Paper>
@@ -75,3 +51,13 @@ export const ColorPickerCell = ({ row }: { row: PointFiltersTableRowEntry }) => 
     </>
   );
 };
+
+const styles = (theme: Theme) => ({
+  geneColorButton: {
+    padding: '4px'
+  },
+  paper: {
+    backgroundColor: alpha(theme.palette.gx.primary.black, 0.75),
+    padding: '8px'
+  }
+});
--- src/components/ViewController/TranscriptLayerSection/PointsFilter/PointFiltersTable/PointFiltersTable.types.ts
+++ src/components/ViewController/TranscriptLayerSection/PointsFilter/PointFiltersTable/PointFiltersTable.types.ts
@@ -2,3 +2,4 @@ import { GxFilterTableRowPropBase } from '../../../../../shared/components/GxFil
 import { ColorMapEntry } from '../../../../../stores/BinaryFilesStore';
 
 export type PointFiltersTableRowEntry = GxFilterTableRowPropBase & ColorMapEntry;
+export type ColorPickerCellProps = { row: PointFiltersTableRowEntry };
