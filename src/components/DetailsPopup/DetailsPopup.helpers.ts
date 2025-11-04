cc14fd33085dad37630a19c7f8ddd762216fb213
pdabrowsky <rullox13@gmail.com>
2025-11-04 14:09:25 +0100

    fix: format arrays in general metadata file

--- src/components/DetailsPopup/DetailsPopup.helpers.ts
+++ src/components/DetailsPopup/DetailsPopup.helpers.ts
@@ -5,6 +5,8 @@ export const formatDetailsPopupData = (section: Record<string, any>): { label: s
     // Round floats to max 2 decimal places
     if (typeof value === 'number' && !Number.isInteger(value)) {
       formattedValue = Math.round(value * 100) / 100;
+    } else if (Array.isArray(value)) {
+      formattedValue = value.join(', ');
     } else if (typeof value === 'object') {
       formattedValue = JSON.stringify(value);
     }
