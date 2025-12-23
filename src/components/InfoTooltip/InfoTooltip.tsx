ddb5d5f8b243d29b79d50c01bb51c538751b1b70
pdabrowsky <rullox13@gmail.com>
2025-12-23 18:41:38 +0100

    feat: add info tooltip

---
+++ src/components/InfoTooltip/InfoTooltip.tsx
@@ -0,0 +1,26 @@
+import { IconButton, Tooltip } from '@mui/material';
+import { InfoOutlined } from '@mui/icons-material';
+import { InfoTooltipProps } from './InfoTooltip.types';
+
+export const InfoTooltip = ({ title, size = 'small' }: InfoTooltipProps) => {
+  return (
+    <Tooltip
+      title={title}
+      arrow
+      placement="top"
+    >
+      <IconButton
+        size={size}
+        sx={sx.infoButton}
+      >
+        <InfoOutlined fontSize={size} />
+      </IconButton>
+    </Tooltip>
+  );
+};
+
+const sx = {
+  infoButton: {
+    padding: '4px'
+  }
+};
