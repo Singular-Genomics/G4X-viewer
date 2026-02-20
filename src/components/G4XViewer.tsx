2d5afb8e04ecce67e2c572982b7302e849c44ae9
pdabrowsky <rullox13@gmail.com>
2026-02-20 11:52:40 +0100

    fix: update background color, remove gradient

--- src/components/G4XViewer.tsx
+++ src/components/G4XViewer.tsx
@@ -1,12 +1,11 @@
-import { Box, Theme, useTheme } from '@mui/material';
+import { Box } from '@mui/material';
 import { useState, useRef, useEffect } from 'react';
 import { Navigation, NavigationView } from './Navigation';
 import { ViewerView } from '../views/ViewerView';
 import { DashboardView } from '../views/DashboardView';
 
 export default function G4XViewer() {
-  const theme = useTheme();
-  const sx = styles(theme);
+  const sx = styles();
 
   const [currentView, setCurrentView] = useState<NavigationView>('viewer');
   const [dashboardScrollPosition, setDashboardScrollPosition] = useState(0);
@@ -61,9 +60,9 @@ export default function G4XViewer() {
   );
 }
 
-const styles = (theme: Theme) => ({
+const styles = () => ({
   mainContainer: {
-    background: `linear-gradient(0deg, ${theme.palette.gx.darkGrey[500]}, ${theme.palette.gx.darkGrey[100]})`,
+    background: 'rgb(38, 40, 42)',
     minHeight: '100dvh',
     width: '100vw',
     display: 'flex',
