2c1fbc32f623d9a0884a1c64f3bdba1e87ba6608
pdabrowsky <rullox13@gmail.com>
2026-02-23 18:15:45 +0100

    fix: make overview smaller on mobile

--- src/components/PictureInPictureViewerAdapter/PictureInPictureViewerAdapter.tsx
+++ src/components/PictureInPictureViewerAdapter/PictureInPictureViewerAdapter.tsx
@@ -1,7 +1,7 @@
 import { AdditiveColormapExtension, DETAIL_VIEW_ID, getDefaultInitialViewState, LensExtension } from '@hms-dbmi/viv';
 import { useChannelsStore } from '../../stores/ChannelsStore/ChannelsStore';
 import { useShallow } from 'zustand/react/shallow';
-import { DEFAULT_OVERVIEW, FILL_PIXEL_VALUE } from '../../shared/constants';
+import { DEFAULT_OVERVIEW, DEFAULT_OVERVIEW_MOBILE, FILL_PIXEL_VALUE } from '../../shared/constants';
 import { useViewerStore } from '../../stores/ViewerStore/ViewerStore';
 import { Box, useMediaQuery, useTheme } from '@mui/material';
 import {
@@ -188,7 +188,7 @@ export const PictureInPictureViewerAdapter = ({ isViewerActive = true }: Picture
             channelsVisible={channelsVisible}
             loader={loader}
             selections={selections}
-            overview={DEFAULT_OVERVIEW}
+            overview={isDesktop ? DEFAULT_OVERVIEW : DEFAULT_OVERVIEW_MOBILE}
             overviewOn={isOverviewOn && !isPolygonDrawingEnabled}
             height={containerSize.height}
             width={containerSize.width}
