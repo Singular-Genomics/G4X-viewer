65265955d455fab0898735711ab5c5df55265e23
pdabrowsky <rullox13@gmail.com>
2026-03-19 12:32:31 +0100

    refactor: add tooltip title

--- src/components/Tooltip/Tooltip.helpers.tsx
+++ src/components/Tooltip/Tooltip.helpers.tsx
@@ -4,9 +4,10 @@ import { t } from 'i18next';
 
 export function TooltipTranscriptConent({ data }: { data: TranscriptDatapointType }) {
   const tooltipItems: TooltipContentItem[] = [
+    { label: t('tooltip.objectLabel'), value: t('tooltip.objectTranscript') },
     {
       label: t('transcript.position'),
-      value: `X: ${data.position[0].toFixed(2)} 
+      value: `X: ${data.position[0].toFixed(2)}
           Y: ${data.position[1].toFixed(2)}`
     },
     { label: t('transcript.geneName'), value: data.geneName },
@@ -18,6 +19,7 @@ export function TooltipTranscriptConent({ data }: { data: TranscriptDatapointTyp
 
 export function TooltipCellMaskContent({ data }: { data: CellMaskDatapointType }) {
   const tooltipItems: TooltipContentItem[] = [
+    { label: t('tooltip.objectLabel'), value: t('tooltip.objectCell') },
     { label: t('segmentation.cellId'), value: data.cellId },
     { label: t('segmentation.clusterId'), value: data.clusterId },
     { label: t('segmentation.area'), value: data.area },
