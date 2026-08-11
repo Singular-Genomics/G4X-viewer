54b6a78d0f4557edae088f343d589dd2db452cbc
pdabrowsky <rullox13@gmail.com>
2026-08-11 14:49:14 +0200

    feat: warn user when ROI drawing hides the overview minimap

--- src/components/ViewController/ChannelsSettingsSection/OverviewToggle/OverviewToggle.tsx
+++ src/components/ViewController/ChannelsSettingsSection/OverviewToggle/OverviewToggle.tsx
@@ -1,25 +1,62 @@
-import { FormControlLabel } from '@mui/material';
+import { Box, FormControlLabel, Theme, Tooltip, tooltipClasses } from '@mui/material';
+import WarningIcon from '@mui/icons-material/Warning';
 import { useViewerStore } from '../../../../stores/ViewerStore/ViewerStore';
+import { usePolygonDrawingStore } from '../../../../stores/PolygonDrawingStore';
 import { useShallow } from 'zustand/react/shallow';
 import { GxCheckbox } from '../../../../shared/components/GxCheckbox';
 import { useTranslation } from 'react-i18next';
+
+const OverviewHiddenWarning = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Tooltip
+      title={t('channelSettings.overviewDisabledWhileDrawing')}
+      placement="top"
+      arrow
+      slotProps={{ popper: { sx: sx.warningTooltip } }}
+    >
+      <WarningIcon sx={sx.warningIcon} />
+    </Tooltip>
+  );
+};
 
 export const OverviewToggle = () => {
   const { t } = useTranslation();
   const [isOverviewOn, toggleOverview] = useViewerStore(
     useShallow((store) => [store.isOverviewOn, store.toggleOverview])
   );
+  const isPolygonDrawingEnabled = usePolygonDrawingStore((store) => store.isPolygonDrawingEnabled);
 
   return (
-    <FormControlLabel
-      label={t('channelSettings.overview')}
-      control={
-        <GxCheckbox
-          onChange={toggleOverview}
-          checked={isOverviewOn}
-          disableTouchRipple
-        />
-      }
-    />
+    <Box sx={sx.wrapper}>
+      <FormControlLabel
+        label={t('channelSettings.overview')}
+        control={
+          <GxCheckbox
+            onChange={toggleOverview}
+            checked={isOverviewOn}
+            disableTouchRipple
+          />
+        }
+      />
+      {isOverviewOn && isPolygonDrawingEnabled && <OverviewHiddenWarning />}
+    </Box>
   );
+};
+
+const sx = {
+  wrapper: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  warningIcon: {
+    color: (theme: Theme) => theme.palette.gx.accent.darkGold,
+    border: ''
+  },
+  warningTooltip: {
+    [`&.${tooltipClasses.popper}[data-popper-placement*="top"] .${tooltipClasses.tooltip}`]: {
+      marginBottom: '0px'
+    }
+  }
 };
